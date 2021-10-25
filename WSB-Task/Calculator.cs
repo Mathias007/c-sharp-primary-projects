6aba820796aee1666eb982aeba74caf761cc0980
Mathias007 <mathias.najlepszy@gmail.com>
2021-10-25 20:11:09 +0200

    Add info in comment, extend readability, make some corrections.

--- WSB-Task/Calculator.cs
+++ WSB-Task/Calculator.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿
+/* Projekt Kalkulatora przygotowany na zajęcia z Podstaw Programowania sem. I WSB (2021/2022).
+ * Autor rozwiązania: Mateusz Stawowski (https://github.com/Mathias007).
+ * Link do repozytorium: https://github.com/Mathias007/WSB-Task.
+ * Kalkulator wykonuje wszystkie operacje wymienione w poleceniu.
+ * Wykorzystuje różne typy zmiennych, instrukcji warunkowych, pętli. Zapewniona obsługa podstawowych wyjątków oraz możliwość powtarzania obliczeń.
+ * Ponieważ program nie korzysta z metod/klas, dyrektywa DRY nie została w pełni zrealizowana, a poszczególne funkcjonalności nie zostały od siebie oddzielone, aczkolwiek starałem się w miarę możliwości unikać powtórzeń i racjonalizować kod.
+ */
+
+using System;
 
 namespace WSB_Task
 {
@@ -8,21 +17,21 @@ namespace WSB_Task
         {
             bool isProgramActive = true;
 
-            Console.WriteLine("Witaj w aplikacji Kalkulator");
+            Console.WriteLine("Witaj w aplikacji Kalkulator!");
 
             while (isProgramActive)
             {
                 double firstNumber;
                 double secondNumber;
 
-                Console.WriteLine("Podaj dwie liczby, które chciałbyś poddać działaniu.");
-                Console.Write("Pierwsza liczba: ");
-                firstNumber = double.Parse(Console.ReadLine());
+                Console.WriteLine("\n Podaj dwie liczby, które chciałbyś poddać działaniu.");
+                Console.Write("     Pierwsza liczba: ");
+                firstNumber = double.Parse(Console.ReadLine().Replace('.', ','));
 
-                Console.Write("Druga liczba: ");
-                secondNumber = double.Parse(Console.ReadLine());
+                Console.Write("     Druga liczba: ");
+                secondNumber = double.Parse(Console.ReadLine().Replace('.', ','));
 
-                Console.WriteLine($"Wybrałeś liczby: {firstNumber} oraz {secondNumber}");
+                Console.WriteLine($"Wybrałeś liczby: {firstNumber} oraz {secondNumber}. \n");
 
                 string[] calcOperations = new string[] { "Dodawanie", "Odejmowanie", "Mnożenie", "Dzielenie", "Potęgowanie", "Pierwiastkowanie", "Procenty" };
                 Console.WriteLine("Jakie działanie chcesz wykonać? Wybierz odpowiednie działanie z listy poniżej:");
@@ -31,8 +40,9 @@ namespace WSB_Task
                         Console.WriteLine($"       {i} - {calcOperations[i].ToUpper()}");
                     }
 
+                Console.Write("     Twój wybór: ");
                 var choosenOperator = int.Parse(Console.ReadLine());
-                Console.WriteLine($"Wybrałeś {calcOperations[choosenOperator].ToUpper()}. ");
+                Console.WriteLine($"Wybrałeś {calcOperations[choosenOperator].ToUpper()}. \n");
 
                 string finalMessage;
                 double operationResult;
@@ -51,7 +61,7 @@ namespace WSB_Task
 
                     case 2:
                         operationResult = firstNumber * secondNumber;
-                        finalMessage = $"Wynik dodawania: {operationResult}";
+                        finalMessage = $"Wynik mnożenia: {operationResult}";
                         break;
 
                     case 3:
@@ -62,52 +72,54 @@ namespace WSB_Task
                             }
                         else
                             {
-                                throw new Exception("Nie można dzielić przez zero!");
+                                throw new DivideByZeroException("Nie można dzielić przez zero!");
                             }
 
                         break;
 
                     case 4:
                         Console.WriteLine($"Którą z wpisanych wcześniej liczb chciałbyś poddać podniesieniu do kwadratu? ");
-                        Console.WriteLine(" 1 - Pierwszą \n 2 - Drugą");
-                        int numberSelectedToOperation = int.Parse(Console.ReadLine());
+                        Console.WriteLine("     1 - Pierwszą \n     2 - Drugą");
+                        Console.Write("     Twój wybór: ");
+                        int numberSelectedToPow = int.Parse(Console.ReadLine());
 
-                        if (numberSelectedToOperation == 1)
+                        if (numberSelectedToPow == 1)
                             {
                                 operationResult = Math.Pow(firstNumber, 2);
                                 finalMessage = $"Wynik potęgowania: {operationResult}";
 
                             }
-                        else if (numberSelectedToOperation == 2)
+                        else if (numberSelectedToPow == 2)
                             {
                                 operationResult = Math.Pow(secondNumber, 2);
                                 finalMessage = $"Wynik potęgowania: {operationResult}";
                             }
                         else
                             {
-                                throw new Exception("Nieprawidłowy wybór!");
+                                throw new NotSupportedException("Nieprawidłowy wybór!");
                             }
 
                         break;
 
                     case 5:
                         Console.WriteLine($"Którą z wpisanych wcześniej liczb chciałbyś mu poddać w celu uzyskania pierwiastka kwadratowego?");
-                        Console.WriteLine(" 1 - Pierwszą \n 2 - Drugą");
-                        numberSelectedToOperation = int.Parse(Console.ReadLine());
+                        Console.WriteLine("     1 - Pierwszą \n     2 - Drugą");
+                        Console.Write("     Twój wybór: ");
+                        int numberSelectedToSqrt = int.Parse(Console.ReadLine());
 
-                        if (numberSelectedToOperation == 1)
+                        if (numberSelectedToSqrt == 1)
                           {
                                 operationResult = Math.Sqrt(firstNumber);
                                 finalMessage = $"Wynik pierwiastkowania: {operationResult}";
                            }
-                        else if (numberSelectedToOperation == 2)
+                        else if (numberSelectedToSqrt == 2)
                            {
                                 operationResult = Math.Sqrt(secondNumber);
                                 finalMessage = $"Wynik pierwiastkowania: {operationResult}";
                            }
                         else
                             {
-                                throw new Exception("Nieprawidłowy wybór!");
+                                throw new NotSupportedException("Nieprawidłowy wybór!");
                             }
                         
                         break;
@@ -128,12 +140,13 @@ namespace WSB_Task
                         break;
 
                     default:
-                        throw new Exception("Nieprawidłowy operator!");
+                        throw new NotSupportedException("Nieprawidłowy operator!");
                 }
 
                 Console.WriteLine(finalMessage);
 
-                Console.WriteLine("Czy chcesz kontynuować? Jeżeli tak, naciśnij Y. Jeżeli nie - wybierz dowolny klawisz, aby zakończyć pracę kalkulatora");
+                Console.WriteLine("Czy chcesz kontynuować? Jeżeli tak, naciśnij Y. Jeżeli nie - wybierz dowolny klawisz, aby zakończyć pracę kalkulatora.");
+                Console.Write("    Twoja decyzja: ");
                 if (Console.ReadLine().ToUpper() != "Y")
                 {
                     Console.WriteLine("Dziękujemy za skorzystanie z programu.");
