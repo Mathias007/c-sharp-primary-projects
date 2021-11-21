15358e01578661834ce15c07a7e38d3c3f22bcc3
Mathias007 <mathias.najlepszy@gmail.com>
2021-11-21 19:20:21 +0100

    Create NameList projects with primary operation on files and lists.

---
+++ NameList/NameList/NameList/NameList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+/* Program przygotowany na zajęcia z Podstaw Programowania sem. I WSB (2021/2022).
+ * Autor rozwiązania: Mateusz Stawowski (https://github.com/Mathias007).
+ * Link do repozytorium zbiorczego: https://github.com/Mathias007/WSB-Task.
+ * Program wczytuje listę z pliku tekstowego, tworzy z pobranych danych listę, a następnie wyświetla:
+     1. Element 246 z listy
+     2. Tylko osoby o imieniu Anna.
+ * Praktyczne operacje z plikiem i ścieżką, a także zastosowanie list, foreach, metod i lambdy na prostym przykładzie. 
+ */
+
+namespace NameList
+{
+    class NameList
+    {
+        static void Main(string[] args)
+        {
+            int selectedIndex = 246;
+            string nameToFind = "Anna";
+            string fileName = "names.txt";
+            var filePath = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, $"..\\..\\..\\{fileName}"));
+
+            List<string> namesMainList = File.ReadLines(filePath).ToList();
+
+            Console.WriteLine($"1. Na pozycji {selectedIndex} listy znajduje się: {namesMainList[selectedIndex]}.");
+
+            Console.WriteLine($"2. Poniżej wyświetlam wszystkie osoby o imieniu {nameToFind}: ");
+            List<string> selectedNamesList = namesMainList.FindAll(line => line.Contains(nameToFind));
+
+            foreach (var name in selectedNamesList)
+            {
+              Console.WriteLine(name);
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
