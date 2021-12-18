e5a3b6468c15b429b5165b8a17faf8bb9b4eaf41
Mathias007 <mathias.najlepszy@gmail.com>
2021-12-18 10:25:20 +0100

    Create Lambda project.

---
+++ Lambda/Lambda/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Podstawy Programowania sem. I WSB (2021/2022) - wyświetlanie wyniku dla drzewa z wyrażenia lambda.
+ * Autor rozwiązania: Mateusz Stawowski (https://github.com/Mathias007).
+ * Link do zbiorczego repozytorium: https://github.com/Mathias007/WSB-Task.
+ */
+
+using System;
+using System.Linq.Expressions;
+
+namespace Lambda
+{
+    class Lambda
+    {
+        static void Main(string[] args)
+        {
+            // input
+            var a = 10;
+            Expression<Func<int>> sum = () => 1 + a + 3 + 4;
+
+            // solution
+            var func = sum.Compile();
+            var result = func();
+
+            // output
+            Console.WriteLine($"Wynik dla drzewa z wyrażenia lambda: {result}");
+        }
+    }
+}
